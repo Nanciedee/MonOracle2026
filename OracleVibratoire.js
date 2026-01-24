6011eae01c1ec84b1a7ef2ab47e5f7debc3244fc
Nanciedee <nanciedee001@gmail.com>
2026-01-24 04:15:31 -0500

    Update OracleVibratoire.js

--- OracleVibratoire.js
+++ OracleVibratoire.js
@@ -7,34 +7,76 @@ const OracleVibratoire = {
     // 1. BASE DE DONNÉES DES CORRESPONDANCES
     data: {
         chakras: [
-            { id: 1, nom: "Racine", freq: "396Hz", couleurs: "Rouge & Noir", img: "img/chakras/racine.jpg" },
-            { id: 2, nom: "Sacré", freq: "417Hz", couleurs: "Orange & Corail", img: "img/chakras/sacre.jpg" },
-            { id: 3, nom: "Plexus Solaire", freq: "528Hz", couleurs: "Jaune & Or", img: "img/chakras/plexus.jpg" },
-            { id: 4, nom: "Cœur", freq: "639Hz", couleurs: "Vert & Rose", img: "img/chakras/coeur.jpg" },
-            { id: 5, nom: "Gorge", freq: "741Hz", couleurs: "Bleu & Argent", img: "img/chakras/gorge.jpg" },
-            { id: 6, nom: "Troisième Œil", freq: "852Hz", couleurs: "Indigo", img: "img/chakras/3oeil.jpg" },
-            { id: 7, nom: "Couronne", freq: "963Hz", couleurs: "Violet & Blanc", img: "img/chakras/couronne.jpg" }
+            { id: 1, nom: "Racine", freq: "396Hz", couleurs: "Rouge & Noir", img: "img/racine.jpg" },
+            { id: 2, nom: "Sacré", freq: "417Hz", couleurs: "Orange & Corail", img: "img/sacre.jpg" },
+            { id: 3, nom: "Plexus Solaire", freq: "528Hz", couleurs: "Jaune & Or", img: "img/plexus.jpg" },
+            { id: 4, nom: "Cœur", freq: "639Hz", couleurs: "Vert & Rose", img: "img/coeur.jpg" },
+            { id: 5, nom: "Gorge", freq: "741Hz", couleurs: "Bleu & Argent", img: "img/gorge.jpg" },
+            { id: 6, nom: "Troisième Œil", freq: "852Hz", couleurs: "Indigo", img: "img/3oeil.jpg" },
+            { id: 7, nom: "Couronne", freq: "963Hz", couleurs: "Violet & Blanc", img: "img/couronne.jpg" }
         ],
         archanges: {
-            "Michaël": { freq: "528Hz", pierres: ["Lapis-lazuli", "Sodalite"], img: "img/archanges/michael.jpg" },
-            "Raphaël": { freq: "639Hz", pierres: ["Malachite", "Émeraude"], img: "img/archanges/raphael.jpg" },
-            "Gabriel": { freq: "741Hz", pierres: ["Pierre de lune", "Citrine"], img: "img/archanges/gabriel.jpg" },
-            "Uriel": { freq: "852Hz", pierres: ["Ambre", "Pyrite"], img: "img/archanges/uriel.jpg" }
+            "Michaël": { freq: "528Hz", pierres: ["Lapis-lazuli", "Sodalite"], img: "img/michael.jpg" },
+            "Raphaël": { freq: "639Hz", pierres: ["Malachite", "Émeraude"], img: "img/raphael.jpg" },
+            "Gabriel": { freq: "741Hz", pierres: ["Pierre de lune", "Citrine"], img: "img/gabriel.jpg" },
+            "Uriel": { freq: "852Hz", pierres: ["Ambre", "Pyrite"], img: "img/uriel.jpg" }
             // ... Ajoutez les autres selon votre liste
         },
         archetypes: [
-            { nom: "Le Visionnaire", element: "Air", msg: "Je vois au-delà du présent.", img: "img/archetypes/visionnaire.jpg" },
-            { nom: "Le Guérisseur", element: "Eau", msg: "Mon cœur connaît la voie.", img: "img/archetypes/guerisseur.jpg" },
-            { nom: "L’Initiateur", element: "Feu", msg: "Je suis le feu du commencement.", img: "img/archetypes/initiateur.jpg" },
-            { nom: "La Gardienne", element: "Terre", msg: "Mon être est refuge.", img: "img/archetypes/gardienne.jpg" }
+            { nom: "Le Visionnaire", element: "Air", msg: "Je vois au-delà du présent.", img: "img/visionnaire.jpg" },
+            { nom: "Le Guérisseur", element: "Eau", msg: "Mon cœur connaît la voie.", img: "img/guerisseur.jpg" },
+            { nom: "L’Initiateur", element: "Feu", msg: "Je suis le feu du commencement.", img: "img/initiateur.jpg" },
+            { nom: "La Gardienne", element: "Terre", msg: "Mon être est refuge.", img: "img/gardienne.jpg" }
         ],
         maitresses: {
-            "Terre": { nom: "Alignement & Identité", img: "img/maitresses/terre.jpg", chakras: [1, 4] },
-            "Feu": { nom: "Courage & Affirmation", img: "img/maitresses/feu.jpg", chakras: [3, 1] },
-            "Air": { nom: "Indépendance & Limites", img: "img/maitresses/air.jpg", chakras: [5, 7] },
-            "Eau": { nom: "Activation du Destin", img: "img/maitresses/eau.jpg", chakras: [2, 4] }
+            "Terre": { nom: "Alignement & Identité", img: "img/terre.jpg", chakras: [1, 4] },
+            "Feu": { nom: "Courage & Affirmation", img: "img/feu.jpg", chakras: [3, 1] },
+            "Air": { nom: "Indépendance & Limites", img: "img/air.jpg", chakras: [5, 7] },
+            "Eau": { nom: "Activation du Destin", img: "img/eau.jpg", chakras: [2, 4] }
         }
     },
+    const CartesComplementaires = {
+    "Amour": {
+        qualite: "Amour Inconditionnel",
+        img: "img/Vibratoire Amour.png",
+        frequence: "639 Hz",
+        pierre: "Quartz rose, Rhodonite",
+        astre: "Vénus",
+        rituel: "Scellement du cœur et harmonie relationnelle."
+    },
+    "Guérison": {
+        qualite: "Régénération Sacrée",
+        img: "img/Vibratoire Guérison.png",
+        frequence: "528 Hz",
+        pierre: "Malachite, Émeraude",
+        astre: "Soleil / Mercure",
+        rituel: "Restauration du corps temple et paix cellulaire."
+    },
+    "Guidance": {
+        qualite: "Intuition & Vision",
+        img: "img/Vibratoire Guide.png",
+        frequence: "741 Hz",
+        pierre: "Pierre de lune, Labradorite",
+        astre: "Lune / Neptune",
+        rituel: "Ouverture des canaux de communication céleste."
+    },
+    "Protection": {
+        qualite: "Force & Souveraineté",
+        img: "img/Vibratoire Protection.png",
+        frequence: "528 Hz / 417 Hz",
+        pierre: "Lapis-lazuli, Tourmaline noire",
+        astre: "Mars / Soleil",
+        rituel: "Bouclier de lumière et transmutation des ombres."
+    },
+    "Sagesse": {
+        qualite: "Connaissance Akashique",
+        img: "img/Vibratoire Sagesse.png",
+        frequence: "852 Hz / 963 Hz",
+        pierre: "Améthyste, Cristal de roche",
+        astre: "Saturne / Uranus",
+        rituel: "Accès aux mémoires de l'âme et clarté divine."
+    }
+};
 
     // 2. LOGIQUE ASTRONOMIQUE SIMPLIFIÉE (Exemple pour 2026)
     getMeteoDuJour: function(date) {
